9a9a48f112d2f766ec6a59449e6efd5bda41c0d2
allyson <allyson.brandao@venhapranuvem.com.br>
2021-06-21 23:44:44 -0300

    Aula 01 - LIFTOFF

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./componentes/Button";
+
+
+function App() {
+  return (
+  <div>
+    <h1> Hello World</h1>
+    <Button text="Button 01"/>
+    <Button text="Button 02"/>
+    <Button/>
+  </div>
+  );
+}
+
+export default App;
